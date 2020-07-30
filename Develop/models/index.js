3e7b7c7ad93cc917863a51180e433abae07a4b9c
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-07-29 23:34:58 -0700

    finished description of models folder

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,36 +1,65 @@
-'use strict';
+'use strict'; //enables "strict mode". With strict mode, you cannot use
+// use undecalred variables. Everything must be strictly defined.
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+var fs        = require('fs'); // requires file system, part of the default npm library
+var path      = require('path'); // requires file system, part of the default npm library
+var Sequelize = require('sequelize'); // requires sequelize, the npm module used by
+// this app to define models and make queries to our database
+var basename  = path.basename(module.filename); // the name of this file. Namely, where 
+// this index.js file is located.
+var env       = process.env.NODE_ENV || 'development'; // states that if a process.env.NODE_ENV
+// exists, which would be the case this app was hosted externally on a platform like heroku,
+// then set env equal to that process.env.NODE_ENV. Otherwise, set it equal to 'development'.
+// which config attributes we select will be dependent on what env is defined as. See ../config/config.json
+// for more details
+var config    = require(__dirname + '/../config/config.json')[env]; // get the values associated
+// with the key "env" in the JSON object located in ../config/config.json.
+var db        = {}; // instantiate our database as an empty object
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
+if (config.use_env_variable) { // if we're using an environment variable, e.g this app
+// is hosted on heroku not our local machine, do the following "if" condition
+  var sequelize = new Sequelize(process.env[config.use_env_variable]); // instantiate a new
+  // Sequelize instance using the parameters in "config" under the key of this env variable
+} else { // if we are not using an environment variable, e.g we're hosting on our local machine.
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // instantiate a new Sequelize instance with the parameters specified in config.json
 }
 
 fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
+  .readdirSync(__dirname) // reads the entire current directory, namely all files in "models".
+  //returns an array of all filenames in the "models" directory.
+
+  .filter(function(file) { // removes all files from the above array that don't meet the
+  //following condition
+
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    // the filter function makes sure each file passes 3 conditions:
+    // 1) the first character of the filename is not ".", meaning that it's not a hidden file
+    // 2) the file is not index.js. We don't want to include index.js since it's not a model
+    // 3) the last 3 characters of the file are ".js". This specifies that the file is a javascript
+    // file. Since our models are contained in javascript files, we make sure the filenames have
+    // this ending.
   })
-  .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
+  .forEach(function(file) { // for each of the files that passed through the filter function above
+    var model = sequelize['import'](path.join(__dirname, file)); // creates a new sequlize model
+    // from the content of each of the files that based the filter function. I.e the files that are
+    // defining sequelize models.
+
+    db[model.name] = model; // sets a key value pair in our db object. For each file that defines a
+    // model, we set a key-value pair in our db object of key = the model's name (in our case, users)
+    // with associated value = the model itself
   });
 
-Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+Object.keys(db).forEach(function(modelName) { // iterate through each key value in our db object
+  if (db[modelName].associate) { // if we have an "associate" field in our model
+    db[modelName].associate(db); // set up the association. Associations include "hasMany", "belongsTo", etc...
   }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize; // "sequelize" = this sequelize instance. This sets a key of "sequelize"
+// in our db object equal to our sequelize instance
 
-module.exports = db;
+db.Sequelize = Sequelize; // "Sequelize" = the npm module sequlize. This declaring that our object "db"
+// is a sequelize database and allows it to be synced to our MySQL database.
+
+module.exports = db; // exports our sequlize database "db" for use in other files
--- Develop/models/user.js
+++ Develop/models/user.js
@@ -1,31 +1,38 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 var bcrypt = require("bcryptjs");
+
 // Creating our User model
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
+// a "model" in sequelize represents a table in a database. Here, we are essentially
+// defining a "users" table in our database.
+module.exports = function(sequelize, DataTypes) { // exports the model for use in other files
+  var User = sequelize.define("User", { // defines a new model of name "User"
     // The email cannot be null, and must be a proper email before creation
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
+    email: { // specfiies that this table has a column named "email"
+      type: DataTypes.STRING, // specifies that the entries to column "email" must be strings
+      allowNull: false, // specifies that the entries to column "email" cannot be null
+      unique: true, // specifies that the entries to column "email" must be unqiue. I.e 
+      // you can't have two email entries in the same table that are the same.
+
+      validate: { // sequelize syntax for setting up validation parameters
+        isEmail: true // built in sequelize validation field. Checks for email
+        // syntax by requiring *something*@*something*.com
       }
     },
     // The password cannot be null
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
+    password: { // specifies that this table has a column named "password"
+      type: DataTypes.STRING, // specifies that the entries to the column "password" must be strings
+      allowNull: false // specifies that the entries to column "password" cannot be null
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
+  User.prototype.validPassword = function(password) { // defines a new method for our model
+    return bcrypt.compareSync(password, this.password); // returns true if the unhashed password entered by the user can be compared to the hashed password stored in our database
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.addHook("beforeCreate", function(user) {
-    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  User.addHook("beforeCreate", function(user) { //makes sure this step is done before creating the model
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null); // built in bcrypt hash function. Hashes the user's
+    // password so that raw passwords are not stored in the database
   });
-  return User;
+  return User; // returns the user model
 };
